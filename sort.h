ea796202eb1cb608ba2768a46eb69199305d0a37
valerienierenberg <valerie.nierenberg@gmail.com>
2021-01-31 18:03:07 +0000

    Add header file

--- sort.h
+++ sort.h
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
@@ -18,20 +32,7 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
-
+int partition(int *arr, size_t low, size_t pivot, size_t size);
+void sort(int *array, size_t start, size_t end, size_t size);
 
 #endif
